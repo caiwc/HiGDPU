e851d07796ef6d94aa39dd4daab53441301fa945
蔡伟城 <caiweicheng@caiweichengdeMacBook-Air.local>
2017-11-30 15:41:52 +0800

    scrapy os 环境配置

--- weixin_scrapy/weixin_scrapy/main.py
+++ weixin_scrapy/weixin_scrapy/main.py
@@ -2,7 +2,7 @@ from scrapy.cmdline import execute
 import sys
 import os
 
-
+os.environ.setdefault('SCRAPY_SETTINGS_MODULE','weixin_scrapy.settings')
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 
