8aea975e4db872e2d9c3b9c6acea7847a76583c7
蔡伟城 <caiweicheng@caiweichengdeMacBook-Air.local>
2017-12-27 20:46:26 +0800

    scrapy sougou验证test

--- weixin_scrapy/weixin_scrapy/verifycode.py
+++ weixin_scrapy/weixin_scrapy/verifycode.py
@@ -25,6 +25,9 @@ def handel_verifycode(url, operation='weixin'):
     driver.get_screenshot_as_file("/tmp/HiGDPU/success.png")
     s_media_id = qyweixin_api.upload_media(qyweixin_api.qyweixin_img_type, "/tmp/HiGDPU/success.png")
     qyweixin_api.send_weixin_message(qyweixin_api.qyweixin_img_type, {'media_id': s_media_id})
+    if operation == 'sougou':
+        driver.find_element_by_css_selector('#sogou_vr_11002301_box_0 .gzh-box2 .txt-box .tit a::attr(href)').click()
+        time.sleep(2)
     driver.quit()
 
 
