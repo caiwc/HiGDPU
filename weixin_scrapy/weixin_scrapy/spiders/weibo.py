66e5e0771e161215bc70d7b2e3a5b927fa0d90cf
蔡伟城 <caiweicheng@caiweichengdeMacBook-Air.local>
2018-01-09 14:32:18 +0800

    fix bugs

--- weixin_scrapy/weixin_scrapy/spiders/weibo.py
+++ weixin_scrapy/weixin_scrapy/spiders/weibo.py
@@ -142,7 +142,7 @@ class WeiboSpider(scrapy.Spider):
         'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36",
     }
 
-    start_page = 852
+    start_page = 1036
 
     re_like = re.compile("赞\[(\d+)]")
     re_report = re.compile("转发\[(\d+)]")
