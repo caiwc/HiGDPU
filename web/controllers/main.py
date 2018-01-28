af22fb615eff1fe4e1cec6486319221d7f1627c9
蔡伟城 <>
2018-01-28 17:51:30 +0800

    test

--- web/controllers/main.py
+++ web/controllers/main.py
@@ -90,10 +90,24 @@ def qyweixin_authorization():
         from_user = xml_tree.find('ToUserName').text
         if content == config.QYWEIXIN_VERIFYCODE:
             res = utils.msg_encrp(wxcpt=wxcpt, to_user=from_user, content='输入url', sReqNonce=sVerifyNonce)
+            print(res)
             response = make_response(res)
             response.content_type = 'application/xml'
             return response
+        else:
+            sRespData = """
+                        <xml>
+                       <ToUserName><![CDATA[{to_user}]]></ToUserName>
+                       <FromUserName><![CDATA[{to_user}]]></FromUserName>
+                       <CreateTime>{timestamp}</CreateTime>
+                       <MsgType><![CDATA[text]]></MsgType>
+                       <Content><![CDATA[{content}]]></Content>
+                       </xml>
+                    """.format(to_user=from_user, timestamp=sVerifyTimeStamp, content=content.encode('utf-8'))
 
+            response = make_response(sRespData)
+            response.content_type = 'application/xml'
+            return response
 # @main_blueprint.route('/login', methods=['GET', 'POST'])
 # @oid.loginhandler
 # def login():
