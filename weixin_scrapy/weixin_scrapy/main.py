9168f9a7ed24a88150370dae86931b47ca961d5e
蔡伟城 <>
2018-03-05 10:30:15 +0800

    fix bugs

--- weixin_scrapy/weixin_scrapy/main.py
+++ weixin_scrapy/weixin_scrapy/main.py
@@ -10,7 +10,7 @@ def run(spider):
     if spider == 'weixin':
         execute(['scrapy', 'crawl', 'weixin'])
     elif spider == 'weibo':
-        execute('scrapy crawl weibo -a start_page=175 -a end_page=500'.split(' '))
+        execute('scrapy crawl weibo -a start_page=1 -a end_page=8'.split(' '))
 
 
 if __name__ == '__main__':
