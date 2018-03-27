b53541321ea19b6f43eba9c9fec6ae2b81a86c48
蔡伟城 <>
2018-03-27 20:42:46 +0800

    fix bugs

--- web/config.py
+++ web/config.py
@@ -100,7 +100,7 @@ class DevConfig(Config):
         },
         'weibo_report': {
             'task': 'web.tasks.weibo_report',
-            'schedule': crontab(day_of_week='2', hour=20, minute=40)
+            'schedule': crontab(day_of_week='2', hour=20, minute=26)
         }
     }
 
